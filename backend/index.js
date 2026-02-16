151519ed47dd903e54a7bc42b6808dd7b4eac439
Sourav kumar <161188554+sourav842741@users.noreply.github.com>
2026-02-16 18:10:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import userRouter from "./routes/user.routes.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://virtual-assistance-5szv.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
